471a25ae7a0c17bcf96e634cf9cc79045af9b7aa
Martin Kleppmann <martin@kleppmann.com>
2015-10-21 22:10:09 +0100

    First attempt at message encoding/decoding

--- spec/crdt/peer_spec.rb
+++ spec/crdt/peer_spec.rb
@@ -16,10 +16,35 @@ RSpec.describe CRDT::Peer do
     end
   end
 
+  def decode_msg(serialized)
+    decoder = Avro::IO::BinaryDecoder.new(StringIO.new(serialized))
+    reader = Avro::IO::DatumReader.new(CRDT::Peer::MESSAGE_SCHEMA)
+    reader.read(decoder)
+  end
+
   it 'should save and reload its state' do
     peer = CRDT::Peer.create
     peer.save(new_tempfile)
     reloaded = CRDT::Peer.load(@tempfiles.first.path)
     expect(reloaded.peer_id).to eq peer.peer_id
+  end
+
+  it 'should send a message from one peer to another' do
+    peer1 = CRDT::Peer.create
+    peer2 = CRDT::Peer.create
+    peer1.local_operation
+    peer2.receive_message(peer1.encode_message.tap {|m| @msg1 = decode_msg(m) })
+    @msg2 = decode_msg(peer2.encode_message)
+
+    expect(@msg1['operations']).to eq([{
+      'referenceID' => nil,
+      'newID' => {'logicalTS' => 0, 'peerIndex' => 0},
+      'value' => 'a'
+    }])
+    expect(@msg2['operations']).to eq([{'updates' => [{
+      'peerID'    => @msg1['origin'],
+      'peerIndex' => 1,
+      'opCount'   => 1
+    }]}])
   end
 end
